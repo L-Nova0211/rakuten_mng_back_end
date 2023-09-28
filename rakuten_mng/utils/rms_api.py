66627de0b761de1454e115ae3817ea23d61eed0d
Raccon <kanazaya74@gmail.com>
2023-09-28 21:01:49 +0900

    add rms api util

---
+++ rakuten_mng/utils/rms_api.py
@@ -0,0 +1,22 @@
+class CabinetAPI:
+    def __init__(self) -> None:
+        pass
+
+    def insert_image(self):
+        pass
+
+
+class ItemAPI:
+    def __init__(self) -> None:
+        pass
+
+    def insert_item(self):
+        pass
+
+
+class InventoryAPI:
+    def __init__(self) -> None:
+        pass
+
+    def register_inventory_stock(self):
+        pass
