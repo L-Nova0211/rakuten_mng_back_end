91e7c50a64b92b7f8fd20c4b30ff1943661b6978
Raccon <kanazaya74@gmail.com>
2023-09-29 00:59:11 +0900

    implement insert_image api

--- rakuten_mng/utils/rms_api.py
+++ rakuten_mng/utils/rms_api.py
@@ -1,9 +1,25 @@
-class CabinetAPI:
-    def __init__(self) -> None:
-        pass
+import base64
+import requests
 
-    def insert_image(self):
-        pass
+
+class CabinetAPI:
+    def __init__(self, service_secret, license_key) -> None:
+        self.main_url = 'https://api.rms.rakuten.co.jp/es/1.0/cabinet'
+        self.auth_key = base64.b64encode(f'{service_secret}:{license_key}'.encode()).decode()
+        self.headers = {
+            'Authorization': f'ESA {self.auth_key}'
+        }
+
+    def insert_image(self, data):
+        url = f'{self.main_url}/file/insert'
+        self.headers['Content-Type'] = 'multipart/form-data'
+        resp = requests.post(
+            headers=self.headers,
+            url=url,
+            data=data,
+            files=data
+        )
+        return resp
 
 
 class ItemAPI:
