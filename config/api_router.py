9320b40b920755b0eb14f62c4bb1ed99fce047ee
Raccon <kanazaya74@gmail.com>
2023-10-18 16:29:05 +0900

    add productsetting viewset

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from rakuten_mng.product.api.views import ProductViewSet
+from rakuten_mng.product.api.views import ProductViewSet, ProductSettingViewSet
 from rakuten_mng.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -11,6 +11,7 @@ else:
 
 router.register("product", ProductViewSet)
 router.register("users", UserViewSet)
+router.register("setting", ProductSettingViewSet)
 
 
 app_name = "api"
