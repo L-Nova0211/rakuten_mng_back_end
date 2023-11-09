aa9c901c36021ddf68090bd82e33c82bad2bf5a8
Raccon <kanazaya74@gmail.com>
2023-11-09 10:23:09 +0900

    fix order app config

--- rakuten_mng/order/apps.py
+++ rakuten_mng/order/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class OrderConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "order"
+    name = "rakuten_mng.order"
+    verbose_name = _("Order")
